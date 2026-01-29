d5f33321370bfd54f3c5e05d672d66d012af3494
Megan Uyehara <uyeharam@uci.edu>
2026-01-28 23:24:11 -0800

    Add comments.

--- messages/rtk_msg.py
+++ messages/rtk_msg.py
@@ -3,7 +3,11 @@ from mavcore.mav_message import MAVMessage, thread_safe
 
 
 class RTKData(MAVMessage):
-    def __init__(self, sequence_num, payload):
+    """
+    Sends RTCM message.
+    """
+
+    def __init__(self, sequence_num: int, payload: bytes):
         super().__init__("RTK_DATA")
         self.sequence_num = sequence_num
         self.data = list(payload)
@@ -13,9 +17,9 @@ class RTKData(MAVMessage):
         return dialect.MAVLink_gps_rtcm_data_message(
             flags=self.sequence_num << 3,
             len=self.data_length,
-            data=self.data + [0 for _ in range(180 - (self.data_length))]
+            data=self.data + [0 for _ in range(180 - (self.data_length))],
         )
-    
+
     @thread_safe
     def __repr__(self):
         return f"""(GPS_RTCM_DATA)\n
@@ -23,20 +27,28 @@ class RTKData(MAVMessage):
             \tlength: {self.data_length}\n
             \tdata: {self.data}"""
 
-"""
-class RTKData(MAVMessage):
-    def __init__(self, is_fragmented: bool, fragment_id: int, sequence_num: int, payload: list[int]):
-        super().__init__("RTK_DATA")
-        self.is_fragmented = is_fragmented
-        self.fragment_id = fragment_id
-        self.sequence_num = sequence_num
-        self.data = payload
-        self.data_length = len(payload)
 
-    def encode(self, system_id, component_id):
-        return dialect.MAVLink_gps_rtcm_data_message(
-            flags=(self.sequence_num << 3) | (self.fragment_id << 1) | self.is_fragmented,
-            len=self.data_length,
-            data=self.data + [0 for _ in range(180 - (self.data_length))]
-        )
-"""
+# class RTKData(MAVMessage):
+#     """
+#     Sends RTCM message.
+#     """
+
+#     def __init__(self, is_fragmented: bool, fragment_id: int, sequence_num: int, payload: list[int]):
+#         super().__init__("RTK_DATA")
+#         self.flags = (sequence_num << 3) | (fragment_id << 1) | is_fragmented
+#         self.data = payload
+#         self.data_length = len(payload)
+
+#     def encode(self, system_id, component_id):
+#         return dialect.MAVLink_gps_rtcm_data_message(
+#             flags=self.flags,
+#             len=self.data_length,
+#             data=self.data + [0 for _ in range(180 - (self.data_length))]
+#         )
+
+#     @thread_safe
+#     def __repr__(self):
+#         return f"""(GPS_RTCM_DATA)\n
+#             \tflags: {bin(self.flags)}\n
+#             \tlength: {self.data_length}\n
+#             \tdata: {self.data}"""
