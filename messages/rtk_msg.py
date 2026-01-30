48c6389a6901f420d0d885ec41ba96704e3a62e9
Philip-Jian <philipsjian@gmail.com>
2026-01-29 18:06:25 -0800

    working fragments

--- messages/rtk_msg.py
+++ messages/rtk_msg.py
@@ -7,48 +7,24 @@ class RTKData(MAVMessage):
     Sends RTCM message.
     """
 
-    def __init__(self, sequence_num: int, payload: bytes):
+    def __init__(self, is_fragmented: bool, fragment_id: int, sequence_num: int, payload: list[int]):
         super().__init__("RTK_DATA")
-        self.sequence_num = sequence_num
-        self.data = list(payload)
+        self.flags = (sequence_num << 3) | (fragment_id << 1) | (1 if is_fragmented else 0)
+        self.data = payload
         self.data_length = len(payload)
 
     def encode(self, system_id, component_id):
+        if self.data_length < 180:
+            self.data = self.data + [0 for _ in range(180 - (self.data_length))]
         return dialect.MAVLink_gps_rtcm_data_message(
-            flags=self.sequence_num << 3,
+            flags=self.flags,
             len=self.data_length,
-            data=self.data + [0 for _ in range(180 - (self.data_length))],
+            data=self.data
         )
 
     @thread_safe
     def __repr__(self):
         return f"""(GPS_RTCM_DATA)\n
-            \tflags: {bin(self.sequence_num << 3)}\n
+            \tflags: {bin(self.flags)}\n
             \tlength: {self.data_length}\n
             \tdata: {self.data}"""
-
-
-# class RTKData(MAVMessage):
-#     """
-#     Sends RTCM message.
-#     """
-
-#     def __init__(self, is_fragmented: bool, fragment_id: int, sequence_num: int, payload: list[int]):
-#         super().__init__("RTK_DATA")
-#         self.flags = (sequence_num << 3) | (fragment_id << 1) | is_fragmented
-#         self.data = payload
-#         self.data_length = len(payload)
-
-#     def encode(self, system_id, component_id):
-#         return dialect.MAVLink_gps_rtcm_data_message(
-#             flags=self.flags,
-#             len=self.data_length,
-#             data=self.data + [0 for _ in range(180 - (self.data_length))]
-#         )
-
-#     @thread_safe
-#     def __repr__(self):
-#         return f"""(GPS_RTCM_DATA)\n
-#             \tflags: {bin(self.flags)}\n
-#             \tlength: {self.data_length}\n
-#             \tdata: {self.data}"""
--- protocols/send_rtk_protocol.py
+++ protocols/send_rtk_protocol.py
@@ -4,10 +4,10 @@ from mavcore.messages.command_ack_msg import CommandAck
 
 GPS_RTCM_MAX_LENGTH = 180
 
-
 class SendRTKProtocol(MAVProtocol):
     """
-    Sends RTK message given it has a payload that is less than or equal to 180 bytes.
+    Sends RTK message and splits it into up to 4 fragments, since payloads have a
+    maximum size of 180 bytes.
     """
 
     def __init__(self, target_system: int = 1, target_component: int = 0):
@@ -15,42 +15,22 @@ class SendRTKProtocol(MAVProtocol):
         self.target_system = target_system
         self.target_component = target_component
         self.sequence_num = 0
+        self.payload = []
 
     def update(self, data):
-        if len(data) < 180:
-            self.rtk_msg = RTKData(self.sequence_num, data)
-            self.sequence_num = (self.sequence_num + 1) % 32
+        self.payload = list(data)
 
     def run(self, sender, receiver):
-        sender.send_msg(self.rtk_msg)
-
-
-# class SendRTKProtocol(MAVProtocol):
-#     """
-#     Sends RTK message and splits it into up to 4 fragments, since payloads have a
-#     maximum size of 180 bytes.
-#     """
-
-#     def __init__(self, target_system: int = 1, target_component: int = 0):
-#         super().__init__()
-#         self.target_system = target_system
-#         self.target_component = target_component
-#         self.sequence_num = 0
-#         self.payload = []
-
-#     def update(self, data):
-#         self.payload = list(data)
-
-#     def run(self, sender, receiver):
-#         fragment_id = 0
-#         while self.payload and fragment_id < 4:
-#             msg_length = min(len(self.payload), GPS_RTCM_MAX_LENGTH)
-#             rtk_msg = RTKData(
-#                 msg_length > GPS_RTCM_MAX_LENGTH,
-#                 fragment_id,
-#                 self.sequence_num,
-#                 self.payload[:msg_length],
-#             )
-#             fragment_id += 1
-#             self.sequence_num = (self.sequence_num + 1) % 32
-#             self.payload = self.payload[msg_length:]
+        fragment_id = 0
+        while self.payload and fragment_id < 4:
+            msg_length = min(len(self.payload), GPS_RTCM_MAX_LENGTH)
+            rtk_msg = RTKData(
+                msg_length > GPS_RTCM_MAX_LENGTH,
+                fragment_id,
+                self.sequence_num,
+                self.payload[:msg_length],
+            )
+            sender.send_msg(rtk_msg)
+            fragment_id += 1
+            self.payload = self.payload[msg_length:]
+        self.sequence_num = (self.sequence_num + 1) % 32
