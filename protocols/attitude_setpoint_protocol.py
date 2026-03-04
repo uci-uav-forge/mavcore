738a9840c339d3f68ea100ad14864e1f71eccdd4
Lab PC <uavforge.uci.sdp@gmail.com>
2026-03-03 18:12:59 -0800

    stufff

--- protocols/attitude_setpoint_protocol.py
+++ protocols/attitude_setpoint_protocol.py
@@ -22,7 +22,10 @@ class AttitudeSetpointProtocol(MAVProtocol):
         self.target_system = target_system
         self.target_component = target_component
 
-        self.q = np.array([0.0, 1.0, 0.0, 0.0])
+        self.transition_time = 2.0
+
+        self.q = np.array([0.815, 0.0, -0.58, 0.0])
+        self.level_q = np.array([1.0, 0.0, 0.0, 0.0])
         self.thrust = 0.1
 
         self.setpoint_msg = SetpointAttitude(
@@ -35,10 +38,33 @@ class AttitudeSetpointProtocol(MAVProtocol):
 
     def run(self, sender, receiver):
         highest = 0.0
+        start_time = time.time()
         while self.current_pos.get_pos_ned()[2] < -200.0:
+            if time.time() - start_time < self.transition_time:
+                new_q = []
+                for i in range(4):
+                    new_q.append(self.level_q[i] + (self.q[i] - self.level_q[i]) * (time.time() - start_time) / self.transition_time)
+                new_q = np.array(new_q)
+                self.setpoint_msg.load_quat_thrust(new_q, self.thrust)
+            else:
+                self.setpoint_msg.load_quat_thrust(self.q, self.thrust)
             sender.send_msg(self.setpoint_msg)
             time.sleep(0.02)
             gs = np.linalg.norm([self.imu.xac, self.imu.yac, self.imu.zac])
             if gs > highest:
                 highest = gs
-            # print(f"highest G: {highest}", flush=True)
+
+        new_start = time.time()
+        while time.time() - new_start < self.transition_time:
+            new_q = []
+            for i in range(4):
+                new_q.append(self.q[i] + (self.level_q[i] - self.q[i]) * (time.time() - new_start) / self.transition_time)
+            new_q = np.array(new_q)
+            self.setpoint_msg.load_quat_thrust(new_q, self.thrust)
+            sender.send_msg(self.setpoint_msg)
+            time.sleep(0.02)
+            gs = np.linalg.norm([self.imu.xac, self.imu.yac, self.imu.zac])
+            if gs > highest:
+                highest = gs
+        
+        print(f"highest G: {highest}", flush=True)
