dab0b5ed387e54cd8f68d5773afd1fca324a4427
tylerdoan2004 <tylerdoan2004@gmail.com>
2026-03-04 17:51:54 -0800

    created two files, send and listen for battery message forwarding

---
+++ dev/listen_battery_test.py
@@ -0,0 +1,15 @@
+import mavcore.mav_device as mav_device
+import mavcore.messages as messages
+import mavcore.protocols as protocols
+import time
+
+device = mav_device.MAVDevice(device_address = "udp:127.0.0.1:14551", source_system = 255, source_component = 10)
+
+# add a listener for the batter status message
+
+bs_msg = messages.BatteryStatus()
+device.add_listener(bs_msg)
+
+while True:
+    print(f"temp: {bs_msg.temp}")
+    time.sleep(5.0)
---
+++ dev/send_battery_test.py
@@ -0,0 +1,15 @@
+import mavcore.mav_device as mav_device
+import mavcore.messages as messages
+import mavcore.protocols as protocols
+import time
+
+device = mav_device.MAVDevice(device_address = "udp:127.0.0.1:14550", source_system = 1, source_component = 10)
+
+
+#create device and send a battery status message protocol
+
+battery_msg = messages.BatteryStatus()
+battery_msg.temp = -12345
+send_battery_status = protocols.UpdateBatteryProtocol(battery_msg)
+
+device.run_protocol(send_battery_status)
